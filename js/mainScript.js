4edd35315f6d0e125a8e56b7b51efb93a81fea71
Hemcode <pro.hemdy@gmail.com>
2023-02-27 19:01:30 +0100

    Hweb_004

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,12 +1,1 @@
-const viewProjects = () => {
-    ul = document.createElement("ul");
-
-    li1 = document.createElement("li");
-    li2 = document.createElement("li");
-
-    body = document.querySelector('body').append(li1);
-    body.append(li2);
-
-    li1.innerHTML = "HKIY";
-    li1.style.fontSize = "22px";
-}
+// Nothing Special
